7a25fb62e9ea8cc4c4fd07922f6c4c744740a935
pat <patgiok@gmail.com>
2022-09-25 00:03:05 -0700

    fixed cars.json, added SSR function to cars id

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -2,7 +2,7 @@
 import { useRouter } from "next/router";
 import Head from 'next/head'
 
-export default function Car({ car }) {
+export default function car({ car }) {
     const router = useRouter();
     const {id} = router.query;
 
@@ -17,12 +17,10 @@ export default function Car({ car }) {
     )
 }
 
+// SSR content generated on server when REQUESTED.
+// ensures get latest data, slower because needs server to respond to requests
 
-// implement getStaticProps inside component file
-// when you build site, next will auto call this function then send
-// to component itself 
-
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
     const req = await fetch(`http://localhost:3000/${params.id}.json`);
     const data = await req.json();
 
@@ -32,22 +30,36 @@ export async function getStaticProps({ params }) {
     }
 }
 
+
+
+// implement getStaticProps inside component file
+// when you build site, next will auto call this function then send
+// to component itself 
+// export async function getStaticProps({ params }) {
+//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//     const data = await req.json();
+
+//     return {
+//         // use props in function component param
+//         props: {car: data},
+//     }
+// }
+
 // next doesnt know how many pages associated to dynamic route,
 // inorder to pre-render all car ids, next needs to know ids in advance.
 // so we implement getstaticpaths()
 // this function can also request data from api or database.
 // returns paths object that contains array with every route for this url
+// export async function getStaticPaths() {
+//     const req = await fetch('http://localhost:3000/cars.json');
+//     const data = await req.json();
 
-export async function getStaticPaths() {
-    const req = await fetch('http://localhost:3000/cars.json');
-    const data = await req.json();
+//     const paths = data.map(car => {
+//         return { params: {id:car }}
+//     })
 
-    const paths = data.map(car => {
-        return { params: {id:car }}
-    })
-
-    return {
-        paths,
-        fallback: false
-    }
-}
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
